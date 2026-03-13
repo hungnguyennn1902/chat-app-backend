b71918b1bb3ef6c1f8aec6f8965310a9c56ffc0f
Neo <neo@gmail.com>
2026-03-13 16:19:00 +0700

    update

--- src/models/User.js
+++ src/models/User.js
@@ -12,5 +12,35 @@ const userSchema = new mongoose.Schema({
     hashedPassword:{
         type:String,
         required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+    displayName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    avatarUrl:{
+        type:String, // CDN Link
+    },
+    avatarId:{
+        type:String, // Cloudinary public_id
+    },
+    bio: {
+        type: String,
+        maxLength: 500
+    },
+    phone:{
+        type:String,
+        sparse:true
     }
+},{
+    timestamps:true
 })
+const User = mongoose.model("User", userSchema)
+export default User
